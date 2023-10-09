d34c66b99c3954d42ce238599788cfa902b137f4
Joshua Randall <jranda10@emich.edu>
2023-10-09 18:39:43 -0400

    allowed choice between class execution

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,32 @@
-
+import java.util.*;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Our team is getting started");
-		Glad.glad();
-		Sad.sad();
+		Scanner scnr = new Scanner(System.in);
+		DisplayMenu();
+		System.out.println();
+		System.out.print("Choice: ");
+		int choice = scnr.nextInt();
+		choose(choice);
+		scnr.close();
 	}
-
+	
+	public static void choose(int choice) {
+		switch(choice) {
+		case 1:
+			Glad.glad();
+			break;
+		case 2:
+			Sad.sad();
+			break;
+		default:
+			System.out.println("Invalid choice");
+			break;
+		}
+	}
+	public static void DisplayMenu() {
+		System.out.println("Choose which method to execute");
+		System.out.println("1. Glad");
+		System.out.println("2. Sad");
+	}
 }
